eefe93fbcfee6e23e99698fc43b6978b563616e9
qwinox <60475943+qwinox@users.noreply.github.com>
2023-07-09 20:45:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,21 +20,10 @@ from IPython.display import Image
 
 def predict(input):
        filename = 'https://raw.githubusercontent.com/qwinox/aml/main/loan.csv'
-       
        dat = pd.read_csv(filename, sep = ',')
-       
-       dat.head(15)
-       
+
        df_encoded = dat.copy()
-       df_encoded.shape
-       
-       df_encoded.isnull().sum()
-       
        df_encoded.Gender.value_counts()
-       
-       df_encoded.head(15)
-       
-       
        le = LabelEncoder()
        
        df_encoded['Gender'] = le.fit_transform(df_encoded['Gender'].values)
@@ -44,12 +33,6 @@ def predict(input):
        df_encoded['Education'] = le.fit_transform(df_encoded['Education'].values)
        df_encoded['Property_Area'] = le.fit_transform(df_encoded['Property_Area'].values)
        df_encoded['Loan_Status'] = le.fit_transform(df_encoded['Loan_Status'].values)
-       
-       df_encoded.head(15)
-       
-       df_encoded.isnull().sum()
-       
-       df_encoded.columns
        
        feature_cols= ['Gender', 'Married', 'Dependents', 'Education',
               'Self_Employed', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
@@ -77,4 +60,4 @@ button_pressed = st.button("Распознать")
 
 if button_pressed:
        user_input = pd.DataFrame({'Gender' : [1], 'Married' : 	[0], 'Dependents' : [0], 'Education' : 	[0], 'Self_Employed' : 	[0], 'ApplicantIncome' : [5849], 'CoapplicantIncome' :	[0.0], 'LoanAmount' : [146.412162], 'Loan_Amount_Term' :	[360.0], 'Credit_History' : [1.0], 'Property_Area' : [2]})
-       st.write("Класс изображения:", predict(user_input)[0])
+       st.write("Класс изображения:", predict(user_input))
