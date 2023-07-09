7aef7e228d7b69f2d75948e84ebcb764496af620
qwinox <60475943+qwinox@users.noreply.github.com>
2023-07-09 20:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import LabelEncoder
+from scikit-learn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
