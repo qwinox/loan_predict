477490bf262df621f4c0088c4b8757410b8f719c
qwinox <60475943+qwinox@users.noreply.github.com>
2023-07-09 20:10:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 import pandas as pd
 import numpy as np
 
-from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
+from sklearn import tree
 
 import matplotlib.pyplot as plt
 
