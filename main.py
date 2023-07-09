a8575fb5fed58393231e8ba125af1cdbfbf823f0
qwinox <60475943+qwinox@users.noreply.github.com>
2023-07-09 20:52:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ import numpy as np
 import sklearn
 
 from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import BaggingClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
 
 
